0053e7d795f57cbd0054c89f92de10eaaad94a06
Guewen Carre <guewencarre@gmail.com>
2023-02-03 15:21:59 +0100

    second commit

--- firebase.js
+++ firebase.js
@@ -4,10 +4,13 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-
-    // YOUR CONFIG
-
-    
+apiKey: "AIzaSyD-Mo5nXXVC0poCWQKj8DzGm5ImJdAsXsQ",
+  authDomain: "authremakefood.firebaseapp.com",
+  projectId: "authremakefood",
+  storageBucket: "authremakefood.appspot.com",
+  messagingSenderId: "487223222755",
+  appId: "1:487223222755:web:9a8766cbe2a1245a48c0d6",
+  measurementId: "G-VKYD65MHCB"
 };
 
 // Initialize Firebase
